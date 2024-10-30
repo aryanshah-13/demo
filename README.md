d2db359eb3f0a6e7a5ef69e080f24397191074f7
Aryan shah <aryanshah2125@gmail.com>
2024-10-30 17:00:06 -0400

    New file added
    Addded a sample java file and modified the Readme file in my local machine

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # This is a demo respository for learning git and GitHub
 
 This is a readme file!
+
+## This is a subheading added locally
+
+This is some description added locally.
---
+++ basicFile.java
@@ -0,0 +1,7 @@
+package demo;
+
+public class basicFile {
+    public static void main(String[] args) {
+        System.out.println("This is the java file in the demo repository.");
+    }
+}
